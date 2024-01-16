3a5e08adc6bc8f537e02904bb31ef66cd624f8af
andrew786e <74216051+andrew786e@users.noreply.github.com>
2024-01-15 23:25:28 -0700

    Completed Multithreaded Program

---
+++ matrixMultiplication.cpp
@@ -0,0 +1,101 @@
+#include <pthread.h>
+#include<iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include "lab1_IO.h"
+#include "timer.h"
+#include <cmath>
+using namespace std;
+
+// P is the same is number of threads 
+int p ;
+
+// n is the size of matrix
+int n ;
+
+// Matrixes
+int** A ;
+int** B;
+int** C;
+
+
+// Performs Matrix Multiplication
+void* threadfunction(void* thread_rank){
+    int* rank = (int*) thread_rank ;
+
+    int x = floor((*rank) / sqrt(p)) ;
+    int y = (*rank) % int(sqrt(p)) ;
+
+    int matrixAUpperBound = (n/sqrt(p) * (x + 1) ) -1 ;
+    int matrixALowerBound = (n/sqrt(p)) * x ;
+
+    int matrixBUpperBound = (n/sqrt(p) * (y + 1) ) -1 ;
+    int matrixBLowerBound = (n/sqrt(p)) * y ;
+
+    //     // Matrix Multiplication
+    for(int i = matrixALowerBound ; i <= matrixAUpperBound ; i++){
+        for(int k = 0 ; k < n ; k++){
+            for(int j = matrixBLowerBound ; j <= matrixBUpperBound ; j++){
+                C[i][j] += A[i][k] * B[k][j] ;
+            }
+        }
+    }
+}
+
+// Perform Matrix Multiplication without the use of threads
+void matrixMultiplicationNormal(int** A , int** B){
+    int D[n][n] ;
+    for(int i = 0 ; i < n ; i++){
+        for(int k = 0 ; k < n ; k++){
+            for(int j = 0; j < n ; j++){
+                D[i][j] += A[i][k] * B[k][j] ;
+            }
+        }
+    }
+}
+
+int main(int argc , char* argv[]){
+    int NTHREADS = atoi(argv[1]) ;
+    pthread_t threadId[NTHREADS] ;
+    int threadRank[NTHREADS]  ;
+    double start , end , nStart , nEnd , timetaken , ntimetaken;
+
+    p = NTHREADS ;
+
+    Lab1_loadinput(&A , &B , &n) ;
+
+    C = new int*[n] ;
+
+    for(int i = 0 ; i < n  ; i++){
+        C[i] = new int[n] ;
+    }
+    
+    for(int i = 0 ; i < n ; i++){
+        for(int k = 0 ; k < n ; k++){
+            C[i][k] = 0 ;
+        }
+    } 
+    GET_TIME(start) ;
+    for(int i = 0 ; i < NTHREADS ; i++){
+        threadRank[i] = i ; 
+        pthread_create(&threadId[i] , NULL , threadfunction  , (void *)&threadRank[i]) ;
+    }
+    
+    for(int j =0; j < NTHREADS; j++){
+        pthread_join( threadId[j], NULL); 
+    }
+    GET_TIME(end) ;
+
+    GET_TIME(nStart) ;
+    matrixMultiplicationNormal(A , B) ;
+    GET_TIME(nEnd) ;
+
+    ntimetaken = nEnd - nStart ;
+    timetaken = end - start ;
+
+    cout << ntimetaken<< endl ;
+    cout << timetaken << endl ;
+
+    Lab1_saveoutput(C , &n , timetaken) ;
+
+}
